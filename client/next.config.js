3783644fd1523621973d32d410ae3a970f55b898
noah <noah071610@naver.com>
2021-05-31 16:45:32 +0900

    optimization

--- client/next.config.js
+++ client/next.config.js
@@ -1,28 +1,3 @@
-// const path = require("path");
-
-// const withBundleAnalyzer = require("@next/bundle-analyzer")({
-//   enabled: process.env.ANALYZE === "true",
-// });
-
-// module.exports = {
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, "styles")],
-//   },
-// };
-
-// module.exports = withBundleAnalyzer({
-//   compress: true,
-//   webpack(config, { webpack }) {
-//     const prod = process.env.NODE_ENV === "production";
-//     return {
-//       ...config,
-//       mode: prod ? "production" : "development",
-//       devtool: prod ? "hidden-source-map" : "eval",
-//       plugins: [...config.plugins],
-//     };
-//   },
-// });
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
