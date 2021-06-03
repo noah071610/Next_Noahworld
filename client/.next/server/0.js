88136ecd3567ed2e590302dd4e4b3eb5fd679399
noah <noah071610@naver.com>
2021-06-04 05:08:53 +0900

    Fixed: Post add Error

---
+++ client/.next/server/0.js
@@ -0,0 +1,43 @@
+exports.ids = [0];
+exports.modules = {
+
+/***/ "/j9D":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _emotion_styled_base__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("w8No");
+/* harmony import */ var _emotion_styled_base__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_emotion_styled_base__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("cDcd");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _emotion_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__("Kps4");
+/* harmony import */ var _emotion_react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_emotion_react__WEBPACK_IMPORTED_MODULE_2__);
+
+
+function _EMOTION_STRINGIFIED_CSS_ERROR__() { return "You have tried to stringify object returned from `css` function. It isn't supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."; }
+
+
+
+
+const Div = _emotion_styled_base__WEBPACK_IMPORTED_MODULE_0___default()("div", true ? {
+  target: "euzpx0s0"
+} : undefined)(true ? {
+  name: "c39870",
+  styles: "padding:1rem 1rem 2rem 1rem;.articles_div{width:90%;margin:0 auto;@media only screen and (max-width: 660px){width:100%;}}"
+} : undefined);
+
+const Articles = ({
+  children
+}) => {
+  return Object(_emotion_react__WEBPACK_IMPORTED_MODULE_2__["jsx"])(Div, {
+    className: "articles"
+  }, Object(_emotion_react__WEBPACK_IMPORTED_MODULE_2__["jsx"])("div", {
+    className: "articles_div"
+  }, children));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Articles);
+
+/***/ })
+
+};;
